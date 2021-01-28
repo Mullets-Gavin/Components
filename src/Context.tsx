2efd51d33b695a658f228b74e804648e29e42aff
Mullets <38087728+Mullets-Gavin@users.noreply.github.com>
2021-01-27 19:00:19 -0500

    Updated context (forgot what oh no)

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,11 +1,14 @@
 import Roact from "@rbxts/roact";
 
+// A map of the pages to have consistency
 export const Pages = {
+	none: 0,
 	shop: 1,
 	inventory: 2,
 	else: 3,
 };
 
+// The context used for page swapping
 export const Context = Roact.createContext({
 	viewIndex: 0,
 	setPage: (index: number) => {},
